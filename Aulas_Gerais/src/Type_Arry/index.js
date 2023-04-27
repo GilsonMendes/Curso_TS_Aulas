563b8da2f4af649812fe92a187e8721a67eac3f4
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-04-27 06:28:51 -0300

    Add aula de arrys

---
+++ Aulas_Gerais/src/Type_Arry/index.js
@@ -0,0 +1,5 @@
+var numeros;
+numeros = [1, 33, 44, 2, 12];
+console.log(numeros);
+numeros.push(55);
+console.log(numeros);
---
+++ Aulas_Gerais/src/Type_Arry/index.ts
@@ -0,0 +1,6 @@
+let numeros: number[];
+numeros = [1, 33, 44, 2, 12];
+console.log(numeros);
+
+numeros.push(55);
+console.log(numeros);
