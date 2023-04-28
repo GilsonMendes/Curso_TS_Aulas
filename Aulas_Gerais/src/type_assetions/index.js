ba5632ce029bec0ef1727b729dd22e5cfcc74756
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-04-27 21:24:43 -0300

    Add aula assetions

---
+++ Aulas_Gerais/src/type_assetions/index.js
@@ -0,0 +1,4 @@
+var statusAtual = 1;
+var mudarStatus = 0;
+mudarStatus = statusAtual;
+console.log(mudarStatus);
---
+++ Aulas_Gerais/src/type_assetions/index.ts
@@ -0,0 +1,7 @@
+let statusAtual: unknown = 1;
+
+let mudarStatus: number = 0;
+
+mudarStatus = statusAtual as number;
+
+console.log(mudarStatus);
