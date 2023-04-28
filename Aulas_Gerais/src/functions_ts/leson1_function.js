7cdfdcbf4d0c8eac056ee5af59d09f3f35e75c36
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-04-27 22:26:38 -0300

    Add aula funções

---
+++ Aulas_Gerais/src/functions_ts/leson1_function.js
@@ -0,0 +1,12 @@
+function login(username) {
+    console.log("Bem vindo ", username);
+}
+function login1(name) {
+    console.log(name);
+}
+login("João");
+login1("Paulo");
+function dadoss(id) {
+    return id;
+}
+console.log(dadoss(23));
---
+++ Aulas_Gerais/src/functions_ts/leson1_function.ts
@@ -0,0 +1,17 @@
+function login(username: string) {
+  console.log("Bem vindo ", username);
+}
+
+function login1(name: string): void {
+  console.log(name);
+}
+
+login("João");
+
+login1("Paulo");
+
+function dadoss(id: number) {
+  return id;
+}
+
+console.log(dadoss(23))
